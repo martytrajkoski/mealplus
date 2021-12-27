034a20113aa005927201096691504c0509580ce5
elena <efilipovska3@gmail.com>
2021-12-27 22:46:46 +0100

    commit

---
+++ Domashna3/mealPlus/src/test/java/com/example/mealPlus/MealPlusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mealPlus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MealPlusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
